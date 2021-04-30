195fbe497d32923c893a4e2305e1cafd6ef80181
Yohei Eguchi <borrow_or_rob.0222@icloud.com>
2021-04-30 21:22:12 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @prototype = Prototype.new
-    @prototypes = Prototype.includes(:user)
   end
 end
